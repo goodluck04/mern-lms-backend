72765acfe035d9c4d006fb07ff21aca3c41d2bd5
goodluck04 <goodluckkumar04@gmail.com>
2023-12-01 17:48:40 +0530

    added role based route protection

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { activateUser, loginUser, logoutUser, registrationUser } from "../cotrollers/user.controller";
-import { isAuthenticated } from "../middleware/auth";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const router = express.Router();
 
 router.post("/registration", registrationUser);
 router.post("/activate-user", activateUser);
 router.post("/login", loginUser);
 router.get("/logout", isAuthenticated, logoutUser);
+// router.get("/logout", isAuthenticated, authorizeRoles("admin"), logoutUser);
 
 export default router;  
