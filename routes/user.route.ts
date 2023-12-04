091fa9956dd1d31267d143c9229a221b3af9e86b
goodluck04 <goodluckkumar04@gmail.com>
2023-12-04 12:57:12 +0530

    user info api

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser, loginUser, logoutUser, registrationUser, updateAccessToken } from "../cotrollers/user.controller";
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, updateAccessToken } from "../cotrollers/user.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.post("/activate-user", activateUser);
 router.post("/login", loginUser);
 router.get("/logout", isAuthenticated, logoutUser);
 router.get("/refresh", updateAccessToken);
+router.get("/me", isAuthenticated, getUserInfo);
 // router.get("/logout", isAuthenticated, authorizeRoles("admin"), logoutUser);
 
 export default router;  
