b5dbe838832e2418b4dcb7fd96bf5a75d0c36be7
goodluck04 <goodluckkumar04@gmail.com>
2023-12-05 09:12:56 +0530

    add question to courseData api

--- cotrollers/course.controller.ts
+++ cotrollers/course.controller.ts
@@ -5,6 +5,7 @@ import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
 import courseModel from "../models/course.model";
 import { redis } from "../utils/redis";
+import mongoose from "mongoose";
 
 // upload course
 export const uploadCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
@@ -162,6 +163,47 @@ export const getCourseByUser = CatchAsyncError(async (req: Request, res: Respons
     }
 })
 
+// add questions in course
+interface IAddQuestionData {
+    question: string;
+    courseId: string;
+    contentId: string;
+}
 
+export const addQuestion = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // get body
+        const { question, courseId, contentId }: IAddQuestionData = req.body;
+        // find the course in db
+        const course = await courseModel.findById(courseId);
+        // search for course content is valid or or not 
+        if (!mongoose.Types.ObjectId.isValid(contentId)) {
+            return next(new ErrorHandler("Invalid content id", 400));
+        }
+        // findthe content id in course
+        const courseContent = course?.courseData?.find((item: any) => item._id.equals(contentId));
+        if (!courseContent) {
+            return next(new ErrorHandler("Invalid content id", 400));
+        }
+        // create a new question object
+        const newQuestion: any = {
+            user: req.user,
+            question,
+            questionReplies: [],
+        };
+        // add new Question object to our content
+        courseContent.questions.push(newQuestion);
+        // save the updated courses
+        await course?.save();
+        // send the response
+        res.status(200).json({
+            success: true,
+            course,
+        });
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+})
 
 
--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../cotrollers/course.controller";
+import { addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../cotrollers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post("/create-course", isAuthenticated, authorizeRoles("admin"), uploadCo
 router.put("/edit-course/:id", isAuthenticated, authorizeRoles("admin"), editCourse);
 router.get("/get-course/:id", getSingleCourse);
 router.get("/get-courses", getAllCourses);
-router.get("/get-course-content/:id", isAuthenticated,getCourseByUser);
+router.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
+router.put("/add-question", isAuthenticated, addQuestion);
 
 export default router;
