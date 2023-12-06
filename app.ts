679c3f31a4e408069a25a96729d9aa7f794e2371
goodluck04 <goodluckkumar04@gmail.com>
2023-12-06 15:12:15 +0530

    layout model schema added

--- app.ts
+++ app.ts
@@ -30,14 +30,15 @@ app.get("/test", (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({
         success: true,
         message: "API is working",
-    })
-})
+    });
+});
+
 
 // unknown routes
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
     const err = new Error(`Route ${req.originalUrl} not found`) as any;
     err.statusCode = 404;
     next(err)
-})
+});
 
 app.use(ErrorMiddleware);
