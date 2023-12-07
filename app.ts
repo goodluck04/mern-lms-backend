5dd3c24f45c4c14b9bdcbf0de57880c7eaad7444
goodluck04 <goodluckkumar04@gmail.com>
2023-12-07 09:41:43 +0530

    layout api created

--- app.ts
+++ app.ts
@@ -9,6 +9,7 @@ import courseRouter from "./routes/course.route"
 import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
 import analyticsRouter from "./routes/analytics.route";
+import layoutRouter from "./routes/layout.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -23,7 +24,7 @@ app.use(
     }))
 
 // routes
-app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRouter, analyticsRouter);
+app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRouter, analyticsRouter, layoutRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
