ea16f9bd30152b3165f186c4973dc6c4e57169cb
goodluck04 <goodluckkumar04@gmail.com>
2023-12-10 10:06:31 +0530

    de

--- app.ts
+++ app.ts
@@ -10,6 +10,7 @@ import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
 import analyticsRouter from "./routes/analytics.route";
 import layoutRouter from "./routes/layout.route";
+  
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
